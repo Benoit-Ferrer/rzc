4d395e3bc5bf739ad99b57cbb6230060fbb2ad87
benoit <ferrer.benoit@reezocar.com>
2019-07-08 12:22:07 +0200

    Update

--- rzc.colors/src/index.js
+++ rzc.colors/src/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'React'
+import React, { Component } from 'react'
 import {Motion, spring} from 'react-motion';
 class ToggleButton extends Component {
   static defaultProps = {
